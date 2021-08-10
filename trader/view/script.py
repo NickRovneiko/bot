aecbc2e9bee66cedbd999b5256d6002442ba9483
user <>
2021-08-10 22:53:00 +0300

    правки

--- trader/view/script.py
+++ trader/view/script.py
@@ -3,8 +3,22 @@ import django
 
 from icecream import ic
 
+import sys
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bot.settings')
+# add your project directory to the sys.path
+project_home = '/home/drann/bot'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# set environment variable to tell django where your settings.py is
+os.environ['DJANGO_SETTINGS_MODULE'] = 'bot.settings'
+
+
+# serve django via WSGI
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+
 django.setup()
 
 

c0edc15e5bb02633223ef22efc91ae5142364163
user <>
2021-08-11 15:56:11 +0300

    главная страница

--- trader/views.py
+++ trader/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from trader.models import Strategy, Position, Trades
+
+
+
+def main(request):
+    list_strats=Strategy.objects.all()
+
+    for strat in list_strats:
+        strat.open=Position.objects.filter(strat=strat.name, active=True).count()
+        strat.closed = Position.objects.filter(strat=strat.name, active=False).count()
+        strat.profit=sum(Position.objects.filter(strat=strat.name, active=False).values_list('profit', flat=True))
+        strat.balance_usd=round(strat.balance_usd)
+
+    list_strats.order_by('-profit')
+
+
+    return render(request, 'trader/main.html',
+                  {'list_strats':list_strats})

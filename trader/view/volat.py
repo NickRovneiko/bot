b5d3be9b83a1ecc79bf72b83ec65e224f32578c5
user <>
2021-08-12 08:30:52 +0300

    правки

--- trader/view/volat.py
+++ trader/view/volat.py
@@ -22,27 +22,42 @@ def attempt(strats):
     Logs(text=f'тикеры {currentPrice}').save()
 
     for strat in strats:
+
+        if strat.balance_usd - strat.step <0 :
+            positions_list = Position.objects.filter(strat=strat.name, active=True)
+            pos = positions_list.order_by('sell_price').last()
+
+            try_sell(currentPrice, strat, pos)
+
         try:
-            try_sell(currentPrice, strat)
+            positions_list = Position.objects.filter(strat=strat.name, active=True)
+            pos = positions_list.order_by('sell_price').first()
+
+            if currentPrice[strat.exchange] > pos.sell_price:
+                try_sell(currentPrice, strat, pos)
+
 
         except:
             pass
 
         try:
-            try_buy(currentPrice, strat)
+
+            positions_list = Position.objects.filter(strat=strat.name, active=True, ).values_list('buy_price',
+                                                                                                  flat=True)
+            min = positions_list.order_by('buy_price').first()
+            max = positions_list.order_by('buy_price').last()
+
+            if not min:
+                min = currentPrice[strat.exchange] + strat.step + 1
+
+            if currentPrice[strat.exchange] < min - strat.step or currentPrice[strat.exchange] > max + strat.step:
+                try_buy(currentPrice, strat)
         except:
             pass
 
 
 def try_buy(currentPrice, strat):
-    positions_list = Position.objects.filter(strat=strat.name, active=True, ).values_list('buy_price', flat=True)
-    min = positions_list.order_by('buy_price').first()
-    max = positions_list.order_by('buy_price').last()
 
-    if not min:
-        min = currentPrice[strat.exchange] + strat.step + 1
-
-    if currentPrice[strat.exchange] < min - strat.step or currentPrice[strat.exchange] > max + strat.step:
         # покупка
         amount_usd = round(strat.amount)
         amount_eth = round(strat.amount / currentPrice[strat.exchange], 6)
@@ -77,49 +92,46 @@ def try_buy(currentPrice, strat):
         )
 
 
-def try_sell(currentPrice, strat):
-    positions_list = Position.objects.filter(strat=strat.name, active=True)
-    pos = positions_list.order_by('sell_price').first()
+def try_sell(currentPrice, strat, pos):
+    # продажа
+    amount_usd = round(pos.amount_eth * currentPrice[strat.exchange], 2)
+    amount_eth = pos.amount_eth
 
-    if currentPrice[strat.exchange] > pos.sell_price:
-        # продажа
-        amount_usd = round(pos.amount_eth * currentPrice[strat.exchange], 2)
-        amount_eth = pos.amount_eth
 
-        Logs(
-            text=f'''создаю трейд {strat.name} - "SELL"- {currentPrice[strat.exchange],
-                                                         amount_usd,
-                                                         -amount_eth,
-                                                         strat.balance_usd + amount_usd,
-                                                         strat.balance_eth - amount_eth}''').save()
-        Trades.objects.create(strat=strat.name,
-                              types='SELL',
-                              price=currentPrice[strat.exchange],
-                              amount_usd=amount_usd,
-                              amount_eth=-amount_eth,
-                              balance_usd=strat.balance_usd + amount_usd,
-                              balance_eth=strat.balance_eth - amount_eth
-                              )
+    Logs(
+        text=f'''создаю трейд {strat.name} - "SELL"- {currentPrice[strat.exchange],
+                                                     amount_usd,
+                                                     -amount_eth,
+                                                     strat.balance_usd + amount_usd,
+                                                     strat.balance_eth - amount_eth}''').save()
+    Trades.objects.create(strat=strat.name,
+                          types='SELL',
+                          price=currentPrice[strat.exchange],
+                          amount_usd=amount_usd,
+                          amount_eth=-amount_eth,
+                          balance_usd=strat.balance_usd + amount_usd,
+                          balance_eth=strat.balance_eth - amount_eth
+                          )
 
-        # закрытие позиции
-        Logs(text=f'''выбрал {strat.name} закрыть позицию {pos.strat}, sell_price= {currentPrice[strat.exchange]}, страйк= {pos.sell_price}''').save()
-        
-        Logs(text=f'''закрываю позицию {strat.name, currentPrice[strat.exchange],
-                                      (pos.sell_price - pos.buy_price) * amount_eth - (pos.sell_price + pos.buy_price) * amount_eth * 0.001, 2}''').save()
-        pos.strat = strat.name
-        pos.sell_price = currentPrice[strat.exchange]
-        pos.closed = timezone.now()
-        pos.active = False
-        pos.profit = round(
-            (pos.sell_price - pos.buy_price) * amount_eth - (pos.sell_price + pos.buy_price) * amount_eth * 0.001, 2)
-        pos.save()
+    # закрытие позиции
+    Logs(text=f'''выбрал {strat.name} закрыть позицию {pos.strat}, sell_price= {currentPrice[strat.exchange]}, страйк= {pos.sell_price}''').save()
 
-        # обновление балансов
-        Logs(text=f'''меняю балансы {strat.balance_usd + amount_usd, strat.balance_eth - amount_eth}''').save()
-        Strategy.objects.filter(name=strat.name).update(
-            balance_usd=strat.balance_usd + amount_usd,
-            balance_eth=strat.balance_eth - amount_eth
-        )
+    Logs(text=f'''закрываю позицию {strat.name, currentPrice[strat.exchange],
+                                  (pos.sell_price - pos.buy_price) * amount_eth - (pos.sell_price + pos.buy_price) * amount_eth * 0.001, 2}''').save()
+    pos.strat = strat.name
+    pos.sell_price = currentPrice[strat.exchange]
+    pos.closed = timezone.now()
+    pos.active = False
+    pos.profit = round(
+        (pos.sell_price - pos.buy_price) * amount_eth - (pos.sell_price + pos.buy_price) * amount_eth * 0.001, 2)
+    pos.save()
+
+    # обновление балансов
+    Logs(text=f'''меняю балансы {strat.balance_usd + amount_usd, strat.balance_eth - amount_eth}''').save()
+    Strategy.objects.filter(name=strat.name).update(
+        balance_usd=strat.balance_usd + amount_usd,
+        balance_eth=strat.balance_eth - amount_eth
+    )
 
 #     return min,max
 #

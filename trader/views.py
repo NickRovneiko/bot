8c5756cf3845389250a63b057c552a99819e8ca6
user <>
2021-08-13 09:53:08 +0300

    правки

--- trader/views.py
+++ trader/views.py
@@ -7,14 +7,22 @@ from django.db.models import Sum
 def main(request):
     list_strats=Strategy.objects.all()
 
+    active_list_strats=[]
+    stop_list_strats=[]
+
     for strat in list_strats:
         strat.open=Position.objects.filter(strat=strat.name, active=True).count()
         strat.closed = Position.objects.filter(strat=strat.name, active=False).count()
         strat.profit=round(sum(Position.objects.filter(strat=strat.name, active=False).values_list('profit', flat=True)))
         strat.balance=round(strat.balance_usd - Position.objects.filter(strat=strat.name, active=True).count() * strat.amount +strat.profit)
         strat.range=round(strat.balance_usd/strat.amount*strat.step,1)
-    list_strats.order_by('-profit')
+
+        if strat.balance>strat.amount:
+            active_list_strats.append(strat)
+        else:
+            stop_list_strats.append(strat)
 
 
     return render(request, 'trader/main.html',
-                  {'list_strats':list_strats})
+                  {'list_strats':active_list_strats,
+                   'stop_list_strats':stop_list_strats})

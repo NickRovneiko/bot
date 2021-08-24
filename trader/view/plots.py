d42d948ba91bd4409eac47c8a27ee1a1a8664cc4
user <>
2021-08-24 14:07:13 +0300

    сохранение

--- trader/view/plots.py
+++ trader/view/plots.py
@@ -12,9 +12,6 @@ from icecream import ic
 import pandas as pd
 
 import numpy as np
-import matplotlib
-
-matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
 
@@ -104,71 +101,3 @@ def get_chart_varian(varian_id):
                 output_type='div')
 
     return data
-
-
-def run():
-    pd.options.display.max_columns = None
-
-    exchange = 'binance'
-    pair = 'ETH/USDT'
-
-    start = 1627776000000
-    end = 1629072000000
-    varian = 'ETH/USDT 10_10_1'
-
-    qs = History.objects.filter(exchange=exchange,
-                                pair=pair,
-                                timestamp__gte=start,
-                                timeframe='1m'
-                                ).exclude(timestamp__gte=end)
-    df = read_frame(qs)
-
-    # df = df[['timestamp', 'open', 'close', 'low', 'high']]
-    df = df[['timestamp', 'open']]
-
-    # qs = Position.objects.filter(varian=varian,
-    #                              opened__gte=start,
-    #                              ).exclude(opened__gte=end)
-    #
-    # df_qs = read_frame(qs)
-    #
-    # df_buy = df_qs[['opened', 'buy_price']]
-    # df_buy = df_buy.rename(columns={'opened': 'timestamp'})
-    #
-    # df = df.merge(df_buy, left_on='timestamp', right_on='timestamp', how='left')
-    #
-    # df_sell = df_qs[['closed', 'sell_price']]
-    # df_sell = df_sell.rename(columns={'closed': 'timestamp'})
-    #
-    # df = df.merge(df_sell, left_on='timestamp', right_on='timestamp', how='left')
-    #
-    # df['timestamp'] = df['timestamp'].values.astype(dtype='datetime64[ms]')
-
-    df['SMA_120'] = df.iloc[:, 1].rolling(window=120).mean()
-    df['SMA_360'] = df.iloc[:, 1].rolling(window=360).mean()
-    df.index = df.timestamp
-
-    plt.figure()
-    plt.grid(True)
-    plt.plot(df['open'], label='Price')
-    plt.plot(df['SMA_120'], label='SMA 120')
-    plt.plot(df['SMA_360'], label='SMA 360')
-    plt.show()
-
-    # fig = go.Figure()
-    #
-    # fig.add_trace((go.Ohlc(
-    #     x=df['timestamp'],
-    #     open=df['open'],
-    #     high=df['high'],
-    #     low=df['low'],
-    #     close=df['close'],
-    #     increasing_line_color='cyan', decreasing_line_color='gray'
-    # )))
-    #
-    # fig.add_traces(go.Scatter(x=df['timestamp'], y=df['buy_price'], mode='markers'))
-    # fig.add_traces(go.Scatter(name='Sell', x=df['timestamp'], y=df['sell_price'], mode='markers',
-    #                           marker_color='rgba(255, 182, 193, .9)'))
-    #
-    # fig.update_xaxes(rangeslider_visible=True)
-    # fig.show()

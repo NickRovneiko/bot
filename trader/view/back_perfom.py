ff8577bd6cc921c2342c38c76b0f0755f4478997
user <>
2021-09-21 22:30:32 +0300

    правки

--- trader/view/back_perfom.py
+++ trader/view/back_perfom.py
@@ -123,7 +123,7 @@ def try_sell(d, row):
     row.save()
 
     Trans(varian = d['varian'].name,
-          amount = (row.sell_price-row.buy_price) * row.amount_base,
+          amount = row.sell_price * row.amount_base - (row.sell_price + row.buy_price) * row.amount_base * 0.0005,
           desc=f"продажа по {d['quote']['close']}").save()
 
     return
